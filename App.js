340f817c26cfec907b83db6337ec1a80e90cd426
Victor <58643574+PizzaCat07@users.noreply.github.com>
2023-05-31 16:16:17 -0700

    Created project

---
+++ App.js
@@ -0,0 +1,24 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import {
+  
+  Text,
+
+  View,
+} from 'react-native';
+
+const App = () => {
+  return (
+    <View>
+      <Text>App</Text>
+    </View>
+  );
+};
+
+export default App;
