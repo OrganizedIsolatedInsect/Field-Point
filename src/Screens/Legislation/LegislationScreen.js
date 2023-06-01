677fdbea390af1610907c7e1e3e3cee9f7ae9d80
Victor <victorma07work@gmail.com>
2023-05-31 17:05:08 -0700

    added folder structure

---
+++ src/Screens/Legislation/LegislationScreen.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const LegislationScreen = () => {
+  return (
+    <View>
+      <Text>LegislationScreen</Text>
+    </View>
+  )
+}
+
+export default LegislationScreen
