636e3f517cca791a138c0c9a149816058ba5fa40
WorkAccountLB <realityisasimulation@gmail.com>
2023-06-06 10:58:20 -0700

    First Commit

---
+++ src/Shared/styles.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const Colours = {
+  primary: "#FFFFFF", //check designer style guide for actual colour
+  backgroundColouring: "#000000", //check designer style guide for actual colour
+};
+
+export default StyleSheet.create({
+  background: {
+    backgroundColour: Colours.backgroundColouring,
+  },
+});
