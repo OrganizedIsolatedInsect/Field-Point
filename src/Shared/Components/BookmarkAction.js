7d0fdd2ff8ba18027cc954aba58016453ee7244a
WorkAccountLB <realityisasimulation@gmail.com>
2023-06-14 13:50:16 -0700

    Expand screen and actions

---
+++ src/Shared/Components/BookmarkAction.js
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const BookmarkAction = () => {
+  return (
+    <View>
+      <Text>BookmarkAction</Text>
+    </View>
+  );
+};
+
+export default BookmarkAction;
