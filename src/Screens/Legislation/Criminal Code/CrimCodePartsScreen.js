1874cb677950d4e9430ff7d833ab958f6ed53e9c
Victor <victorma07work@gmail.com>
2023-06-14 12:31:26 -0700

    added CrimCodeSectionScreen

--- src/Screens/Legislation/Criminal Code/CrimCodePartsScreen.js
+++ src/Screens/Legislation/Criminal Code/CrimCodePartsScreen.js
@@ -3,6 +3,9 @@ import React from "react";
 import LegislationPartsButton from "../../../Shared/Components/LegislationPartsButton";
 import styles, { Color } from "../../../Shared/styles";
 
+//screen to house top level of Criminal Code and display all the parts of the legislation
+//parts are seperated by the component LegislationPartsButton, pressing will lead to sections of Crim Code
+
 const CrimCodePartsScreen = () => {
   return (
     <View>
--- src/Shared/Navigation/CrimCodeStackNavigator.js
+++ src/Shared/Navigation/CrimCodeStackNavigator.js
@@ -1,20 +1,25 @@
 import { createStackNavigator } from "@react-navigation/stack";
 
 import CrimCodePartsScreen from "../../Screens/Legislation/Criminal Code/CrimCodePartsScreen";
+import CrimCodeSectionsScreen from "../../Screens/Legislation/Criminal Code/CrimCodeSectionsScreen";
 
 // Create Stack to go though Criminal Code screens. Order should be Criminal Code Parts --> Criminal Code Section --> Criminal Code Content.
 
-const CrimCodeNavigator = createStackNavigator()
+const CrimCodeNavigator = createStackNavigator();
 
 const CrimCodeStack = () => {
-    return(
+  return (
     <CrimCodeNavigator.Navigator>
-        <CrimCodeNavigator.Screen name="CrimCodeParts" component={CrimCodePartsScreen}/>
+      <CrimCodeNavigator.Screen
+        name="CrimCodeParts"
+        component={CrimCodePartsScreen}
+      />
+      <CrimCodeNavigator.Screen
+        name="CrimCodeSections"
+        component={CrimCodeSectionsScreen}
+      />
     </CrimCodeNavigator.Navigator>
-    )
-   
+  );
+};
 
-}
-
-
-export {CrimCodeStack}
+export { CrimCodeStack };
