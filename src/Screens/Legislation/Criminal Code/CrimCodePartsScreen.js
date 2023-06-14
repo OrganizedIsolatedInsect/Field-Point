0db431e7c7e8a03bf52f233745f35fc6ea4c889f
Victor <victorma07work@gmail.com>
2023-06-14 15:01:46 -0700

    database call and display data in buttons

--- src/Screens/Legislation/Criminal Code/CrimCodePartsScreen.js
+++ src/Screens/Legislation/Criminal Code/CrimCodePartsScreen.js
@@ -1,16 +1,53 @@
 import { View, Text } from "react-native";
-import React from "react";
+import React, { useState, useEffect } from "react";
 import LegislationPartsButton from "../../../Shared/Components/LegislationPartsButton";
-import styles, { Color } from "../../../Shared/styles";
+import { crimCodeDb } from "../../../Shared/Functions/Database";
+import { FlatList } from "react-native";
 
 //screen to house top level of Criminal Code and display all the parts of the legislation
 //parts are seperated by the component LegislationPartsButton, pressing will lead to sections of Crim Code
 
 const CrimCodePartsScreen = () => {
+  const [dbData, setDbData] = useState([]); // create array to house data from Crim Code Database
+
+  //function to pull heading1_label and heading1 (part# and heading title) from Crim Code Database
+  const getDbData = () => {
+    crimCodeDb.transaction((tx) => {
+      tx.executeSql(
+        'SELECT DISTINCT Heading1_Label,Heading1 from criminal_code WHERE Heading1_Label LIKE "Part%"',
+        [],
+        (tx, results) => {
+          let tempArray = [];
+          for (let i = 0; i < results.rows.length; i++) {
+            tempArray.push(results.rows.item(i));
+          }
+          setDbData(tempArray);
+        }
+      );
+    });
+  };
+
+  useEffect(() => {
+    getDbData();
+  }, []);
+  /* 
+  dbData.map((x) => {
+    console.log(x);
+  }); */
+
   return (
     <View>
-      <LegislationPartsButton />
-      <LegislationPartsButton />
+      <FlatList
+        data={dbData}
+        renderItem={({ item }) => {
+          return (
+            <LegislationPartsButton
+              headingLabel={item.Heading1}
+              partLabel={item.Heading1_Label}
+            />
+          );
+        }}
+      />
     </View>
   );
 };
--- src/Shared/Components/LegislationPartsButton.js
+++ src/Shared/Components/LegislationPartsButton.js
@@ -5,17 +5,16 @@ import Ionicons from "react-native-vector-icons/Ionicons";
 
 //button created for the parts of legislation, after pushing it will move to the sections portion
 
-const LegislationPartsButton = () => {
+const LegislationPartsButton = ({ headingLabel, partLabel }) => {
   return (
     <View style={styles.partsButton}>
       <Ionicons name="chevron-back-circle" style={styles.partsButtonCheveron} />
       <View>
         <Text style={styles.partsButtonPrimaryText}>
-          LegislationPartsButton
+          {/* Temp fix for blank heading1_label for part I */}
+          {headingLabel != "" ? headingLabel : "General"}
         </Text>
-        <Text style={styles.partsButtonSecondaryText}>
-          LegislationPartsButton
-        </Text>
+        <Text style={styles.partsButtonSecondaryText}>{partLabel}</Text>
       </View>
     </View>
   );
