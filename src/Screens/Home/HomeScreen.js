7adf0fb7dac8a7766c0886871979bd41e5d92eec
WorkAccountLB <realityisasimulation@gmail.com>
2023-06-14 16:56:48 -0700

    not to be implemented

--- src/Screens/Home/HomeScreen.js
+++ src/Screens/Home/HomeScreen.js
@@ -1,12 +1,45 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, Pressable } from "react-native";
+import React, { useState, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { switchMarks } from "../../Shared/Functions/Functions";
+import { Color } from "../../Shared/styles";
+import Icon from "react-native-vector-icons/MaterialIcons";
 
 const HomeScreen = () => {
+  //FOR TESTING PURPOSES
+  const [marked, setMarked] = useState(false); //to change marked status of content
+
+  //pull state to see if current section exists in bookmarks
+  const bookmarkStateId = useSelector(state => state.bookmarks.sections);
+
+  useEffect(() => {
+    // compares state array to see if section exists in bookmarks, if it does turn on bookmark icon
+    if (bookmarkStateId.some(e => e.section == provisionID)) {
+      setMarked(switchMarks(false));
+    } else {
+      setMarked(switchMarks(true));
+    }
+  }, [marked]);
+  console.log(marked);
   return (
     <View>
       <Text>HomeScreen</Text>
+      <Pressable>
+        <Icon
+          name={marked ? "bookmark" : "bookmark-outline"}
+          size={30}
+          onPress={() => {
+            setMarked(switchMarks(marked));
+            //dispatchAction();
+          }}
+          style={{ color: Color.inActiveIcon }}
+        />
+        <Text style={{ color: Color.inActiveIcon }}>marked: {marked}</Text>
+      </Pressable>
     </View>
-  )
-}
+  );
 
-export default HomeScreen
+  //ALL FOR TESTING
+};
+
+export default HomeScreen;
