dfdc4643a6a2ae281ef9bc3ad465475c7d44df30
Victor <victorma07work@gmail.com>
2023-06-14 17:06:10 -0700

    added section styling

--- src/Screens/Legislation/Criminal Code/CrimCodePartsScreen.js
+++ src/Screens/Legislation/Criminal Code/CrimCodePartsScreen.js
@@ -2,27 +2,27 @@ import { View, Text } from "react-native";
 import React, { useState, useEffect } from "react";
 import { FlatList } from "react-native";
 
-import LegislationPartsButton from "../../../Shared/Components/LegislationPartsButton";
+import LegislationPartsAccordion from "../../../Shared/Components/LegislationPartsAccordion";
 import { getDbDataCrimCodeHeading } from "./CriminalCodeFunctions";
 
 //screen to house top level of Criminal Code and display all the parts of the legislation
-//parts are seperated by the component LegislationPartsButton, pressing will lead to sections of Crim Code
+//parts are seperated by the component LegislationPartsAccordion, pressing will lead to sections of Crim Code
 
 const CrimCodePartsScreen = () => {
-  const [dbData, setDbData] = useState([]); // create array to house data from Crim Code Database
+  const [dbDataHeading, setDbDataHeading] = useState([]); // create array to house heading data from Crim Code Database
 
   //function to get heading data from database
   useEffect(() => {
-    getDbDataCrimCodeHeading(setDbData);
+    getDbDataCrimCodeHeading(setDbDataHeading);
   }, []);
 
   return (
     <View>
       <FlatList
-        data={dbData}
+        data={dbDataHeading}
         renderItem={({ item }) => {
           return (
-            <LegislationPartsButton
+            <LegislationPartsAccordion
               headingLabel={item.Heading1}
               partLabel={item.Heading1_Label}
             />
