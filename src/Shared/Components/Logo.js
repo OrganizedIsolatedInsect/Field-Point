18c7c3f447b0b4af378bc0d3f0b9bd495e6f15d9
doolaik <dev@vernlam.com>
2023-06-15 08:19:38 -0700

    Variable dimensions added

--- src/Shared/Components/Logo.js
+++ src/Shared/Components/Logo.js
@@ -2,13 +2,19 @@ import React from "react";
 import { View, Image, StyleSheet } from "react-native";
 
 // Logo is scalable
-// Takes logoHeight and logoWidth as props
+// Takes logoHeight and logoWidth as props. If height and width is undefined, use the defaultLogoSize dimensions.
+//      <Logo logoHeight={200} logoWidth={300} />
 export const Logo = ({ logoHeight, logoWidth }) => {
   return (
     <View>
       <Image
         resizeMode="contain"
-        style={[styles.defaultImage, { height: logoHeight, width: logoWidth }]}
+        style={{
+          height:
+            logoHeight === undefined ? styles.defaultLogoSize.height : logoHeight,
+          width:
+            logoWidth === undefined ? styles.defaultLogoSize.width : logoWidth,
+        }}
         source={require("../../Assets/Imgs/Shared/FP-Logo-547x490.png")}
       />
     </View>
@@ -16,7 +22,7 @@ export const Logo = ({ logoHeight, logoWidth }) => {
 };
 
 const styles = StyleSheet.create({
-  defaultImage: {
+  defaultLogoSize: {
     height: 51,
     width: 57,
   },
