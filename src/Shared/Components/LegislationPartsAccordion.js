df295a47df393556b74fecdb54b76106b4d46a4e
Victor <victorma07work@gmail.com>
2023-06-14 17:12:49 -0700

    added accordion functionality

--- src/Shared/Components/LegislationPartsAccordion.js
+++ src/Shared/Components/LegislationPartsAccordion.js
@@ -2,19 +2,29 @@ import { View, Text } from "react-native";
 import React, { useState, useEffect } from "react";
 import styles from "../styles";
 import Ionicons from "react-native-vector-icons/Ionicons";
+import {
+  Collapse,
+  CollapseHeader,
+  CollapseBody,
+} from "accordion-collapse-react-native";
 
 import { getDbDataCrimCodeSection } from "../../Screens/Legislation/Criminal Code/CriminalCodeFunctions";
 import LegislationSectionsAccordion from "./LegislationSectionsAccordion";
-//button created for the parts of legislation, after pushing it will move to the sections portion
 
 /* 
 Fields from database as follows
 headingLabel = Heading1
 partLabel = Heading1_Label
+
+Accordion instuctions can be found at https://github.com/marouanekadiri/Accordion-Collapse-react-native#readme
  */
 
 const LegislationPartsAccordion = ({ headingLabel, partLabel }) => {
   const [dbDataSection, setDbDataSection] = useState([]); // create array to section heading data from Crim Code Database
+
+  /* 
+ sends partLabel into SQL query in getDbDataCrimCodeSection to get back distinct section data per part
+ */
 
   useEffect(() => {
     getDbDataCrimCodeSection(partLabel, setDbDataSection);
@@ -22,24 +32,32 @@ const LegislationPartsAccordion = ({ headingLabel, partLabel }) => {
 
   return (
     <View>
-      <View style={styles.accordionHeader}>
-        <Ionicons
-          name="chevron-back-circle"
-          style={styles.partsCheveronCircle}
-        />
-        <View style={styles.headingLabelContainer}>
-          <Text style={styles.partsPrimaryText}>
-            {/* Temp fix for blank heading1_label for part I */}
-            {headingLabel != "" ? headingLabel : "General"}
-          </Text>
-          <Text style={styles.partsSecondaryText}>{partLabel}</Text>
-        </View>
-      </View>
-      <View>
-        {dbDataSection.map((x) => {
-          return <LegislationSectionsAccordion sectionHeading={x.Heading2} />;
-        })}
-      </View>
+      <Collapse>
+        <CollapseHeader>
+          <View style={styles.accordionHeader}>
+            <Ionicons
+              name="chevron-back-circle"
+              style={styles.partsCheveronCircle}
+            />
+            <View style={styles.headingLabelContainer}>
+              <Text style={styles.partsPrimaryText}>
+                {/* Temp fix for blank heading1_label for part I */}
+                {headingLabel != "" ? headingLabel : "General"}
+              </Text>
+              <Text style={styles.partsSecondaryText}>{partLabel}</Text>
+            </View>
+          </View>
+        </CollapseHeader>
+        <CollapseBody>
+          <View>
+            {dbDataSection.map((x) => {
+              return (
+                <LegislationSectionsAccordion sectionHeading={x.Heading2} />
+              );
+            })}
+          </View>
+        </CollapseBody>
+      </Collapse>
     </View>
   );
 };
--- src/Shared/Components/LegislationSectionsAccordion.js
+++ src/Shared/Components/LegislationSectionsAccordion.js
@@ -3,6 +3,10 @@ import React from "react";
 import styles from "../styles";
 import Ionicons from "react-native-vector-icons/Ionicons";
 
+/* 
+Fields from database as follows
+sectionHeading = Heading2
+ */
 const LegislationSectionsAccordion = ({ sectionHeading }) => {
   return (
     <Pressable
