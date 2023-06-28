35c49f16c44c84bc6598f7b3781a241a5fc6f47f
WorkAccountLB <realityisasimulation@gmail.com>
2023-06-28 12:39:21 -0700

    Fix component

--- src/Screens/Legislation/Criminal Code/CrimCodePartsScreen.js
+++ src/Screens/Legislation/Criminal Code/CrimCodePartsScreen.js
@@ -1,10 +1,31 @@
-import { View, Text } from "react-native";
-import React from "react";
-
-export default function CrimCodePartsScreen() {
+import React, { useState, useEffect } from "react";
+import { FlatList } from "react-native";
+import LegislationPartsAccordion from "../../../Shared/Components/LegislationPartsAccordion";
+import { getDbDataCrimCodeHeading } from "./CriminalCodeFunctions";
+//screen to house top level of Criminal Code and display all the parts of the legislation
+//parts are seperated by the component LegislationPartsAccordion, pressing will lead to sections of Crim Code
+const CrimCodePartsScreen = () => {
+  const [dbDataHeading, setDbDataHeading] = useState([]); // create array to house heading data from Crim Code Database //function to get heading data from database
+  useEffect(() => {
+    getDbDataCrimCodeHeading(setDbDataHeading);
+  }, []);
   return (
     <View>
-      <Text>CrimCodePartsScreen</Text>
+           {" "}
+      <FlatList
+        data={dbDataHeading}
+        renderItem={({ item }) => {
+          return (
+            <LegislationPartsAccordion
+              headingLabel={item.Heading1}
+              partLabel={item.Heading1_Label}
+              sortIndex={item.SortIndex}
+            />
+          );
+        }}
+      />
+         {" "}
     </View>
   );
-}
+};
+export default CrimCodePartsScreen;
