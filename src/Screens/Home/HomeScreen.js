17c8f656c31487f0e5813a1f680a23268097a524
doolaik <dev@vernlam.com>
2023-07-04 11:23:53 -0700

    Implemented breadcrumb

--- src/Screens/Home/HomeScreen.js
+++ src/Screens/Home/HomeScreen.js
@@ -1,4 +1,4 @@
-import { Text, ScrollView } from "react-native";
+import { Text, View, ScrollView } from "react-native";
 import React from "react";
 import styles from "../../Shared/styles.js";
 import HomeScreenButton from "../../Shared/Components/HomeScreenButton.js";
@@ -33,18 +33,20 @@ let showButtons = HomeScreenData.map(
   }
 );
 
-
 const HomeScreen = ({ navigation }) => {
   return (
-    <ScrollView style={styles.screenBackground}>
-      <Text style={styles.homeScreenHeader}>What are you looking for?</Text>
-      <Text style={styles.homeScreenSubHeader}>
-        Explore supporting information and resources
-      </Text>
+    <ScrollView>
+      <Breadcrumb />
+      <View style={styles.screenBackground}>
+        <Text style={styles.homeScreenHeader}>What are you looking for?</Text>
+        <Text style={styles.homeScreenSubHeader}>
+          Explore supporting information and resources
+        </Text>
 
-      {/* Show the buttons from the <HomeScreenButton /> compoment 
+        {/* Show the buttons from the <HomeScreenButton /> compoment 
       with props mapped from the HomeScreenData.js JSON file */}
-      {showButtons}
+        {showButtons}
+      </View>
     </ScrollView>
   );
 };
