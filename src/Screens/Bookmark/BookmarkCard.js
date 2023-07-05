950907fd1b4a196c899ef4c1a50473bf6ef05a8d
WorkAccountLB <realityisasimulation@gmail.com>
2023-07-05 14:27:08 -0700

    even more comments

--- src/Screens/Bookmark/BookmarkCard.js
+++ src/Screens/Bookmark/BookmarkCard.js
@@ -10,6 +10,14 @@ import styles from "../../Shared/styles";
 import Icon from "../../Shared/Components/Icon.js";
 
 export const BookmarkCard = ({ item }) => {
+  //item passed is a set of props that contain
+  //title, this is the name of the Screen as per the LegislationStackNavigator.
+  //This is used as a lookup to obtain the "short version" of the legislation name for output to the device as well as determine which screen the bookmark will return to.
+  // eg.  "Criminal Code of Canada"
+  //sectionNum, the section number of the specific legislation being passed. eg. 83.05
+  //partLabel, the part of the legislation where the section number sits. eg. PART III
+  //sectionHeading, - the description of the specific legislation.  eg. List of Entities
+  //legislation - this is the short form of the legislation used for display purposes.  eg.  Crim Code
   const navAid = useNavigation();
   return (
     <View>
